e4bba867df8564b05cca567c847fccfa9b8ee4f2
ialsd <14mouse_14@mail.ru>
2023-10-04 23:38:13 +0500

    hotfix

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -9,10 +9,94 @@ float centerXMoon = 1.0;
 float centerYMoon = 0.0;
 
 float angle = 0.0;
-float radius = 1.0;
-int numSegments = 100;
+float orbitRadius = 1.0;
 
 bool isDay = true;
+
+void drawCircle(float radius, float centerX, float centerY){
+    int numSegments = 100;
+    glBegin(GL_POLYGON);
+    for (int i = 0; i < numSegments; i++){
+        float theta = 2.0f * M_PI * float(i) / float(numSegments);
+        float x = radius * cosf(theta);
+        float y = radius * sinf(theta);
+        glVertex2f(x + centerX, y + centerY);
+    }
+    glEnd();
+}
+void drawSquare(float x1, float x2,float y1,float y2){
+    glBegin(GL_QUADS);
+    glVertex2f(x1, y2);
+    glVertex2f(x2, y2);
+    glVertex2f(x2, y1);
+    glVertex2f(x1, y1);
+    glEnd();
+}
+void drawPig(float centerXSheep, float centerYSheep){
+    float earRadius = 0.03;
+    float eyeRadius = 0.01;
+    float noseRadius = 0.02;
+    float nostrilRadius = 0.005;
+    glColor3f(1.0, 0.61, 0.67);
+    drawCircle(radiusSun, centerXSheep, centerYSheep);
+
+    glColor3f(0.87, 0.27, 0.57);
+    drawCircle(earRadius, centerXSheep - 0.05, centerYSheep + 0.05);
+    drawCircle(earRadius, centerXSheep + 0.05, centerYSheep + 0.05);
+
+    glColor3f(0.5, 0.5, 0.5);
+    drawSquare(centerXSheep - 0.04, centerXSheep - 0.02,
+               centerYSheep - 0.07, centerYSheep - 0.12);
+    drawSquare(centerXSheep + 0.02, centerXSheep + 0.04,
+               centerYSheep - 0.07, centerYSheep - 0.12);
+
+    glColor3f(0, 0, 0);
+    drawCircle(eyeRadius, centerXSheep-0.02, centerYSheep);
+    drawCircle(eyeRadius, centerXSheep+0.02, centerYSheep);
+
+    glColor3f(0.87, 0.27, 0.57);
+    drawCircle(noseRadius, centerXSheep, centerYSheep - 0.03);
+
+    glColor3f(0, 0, 0);
+    drawCircle(nostrilRadius, centerXSheep + 0.007, centerYSheep - 0.03);
+    drawCircle(nostrilRadius, centerXSheep - 0.007, centerYSheep - 0.03);
+}
+void drawFlower(float x, float y)
+{
+    glLineWidth(3.0);
+    glColor3f(0.0, 0.5, 0.0);
+    glBegin(GL_LINES);
+    glVertex2f(x, y);
+    glVertex2f(x, y - 0.2);
+    glEnd();
+    glLineWidth(1.0);
+
+    float flowerRadius = 0.05;
+    int numPetals = 6;
+
+    glColor3f(1.0, 0.0, 0.0);
+    for (int i = 0; i < numPetals; i++){
+        float angle = 2.0f * M_PI * i / numPetals;
+        float xOffset = flowerRadius * cos(angle);
+        float yOffset = flowerRadius * sin(angle);
+        glBegin(GL_POLYGON);
+        for (int j = 0; j < 5; j++){
+            float petalAngle = 2.0f * M_PI * (j + i % 2 * 0.5) / 5;
+            float xPetal = xOffset + 0.03 * cos(petalAngle);
+            float yPetal = yOffset + 0.075 * sin(petalAngle);
+            glVertex2f(xPetal + x, yPetal + y);
+        }
+        glEnd();
+    }
+    glColor3f(0.0, 0.5, 0.0);
+    glBegin(GL_POLYGON);
+    glVertex2f(x - 0.03, y - 0.07);
+    glVertex2f(x - 0.08, y - 0.15);
+    glVertex2f(x, y - 0.11);
+    glVertex2f(x + 0.08, y - 0.15);
+    glVertex2f(x + 0.03, y - 0.07);
+    glEnd();
+}
 
 void displayMe(void)
 {
@@ -20,52 +104,44 @@ void displayMe(void)
         glClearColor(0.69, 0.93, 0.93, 1);
     }
     else{
-        glClearColor(0.0, 0.13, 0.22, 1.0); // Черный цвет фона в ночное время
+        glClearColor(0.0, 0.13, 0.22, 1.0);
     }
     glClear(GL_COLOR_BUFFER_BIT);
 
+    //Солнце
     glColor3f(0.98,0.9,0.08);
-    glBegin(GL_POLYGON);
-    for (int i = 0; i < numSegments; i++){
-        float theta = 2.0f * M_PI * float(i) / float(numSegments);
-        float x = radiusSun * cosf(theta);
-        float y = radiusSun * sinf(theta);
-        glVertex2f(x + centerXSun, y + centerYSun);
-    }
-    glEnd();
+    drawCircle(radiusSun, centerXSun, centerYSun);
 
+    //Луна
     glColor3f(1.0, 1.0, 0.6);
-    glBegin(GL_POLYGON);
-    for (int i = 0; i < numSegments; i++){
-        float theta = 2.0f * M_PI * float(i) / float(numSegments);
-        float x = radiusSun * cosf(theta);
-        float y = radiusSun * sinf(theta);
-        glVertex2f(x + centerXMoon, y + centerYMoon);
-    }
-    glEnd();
+    drawCircle(radiusSun, centerXMoon, centerYMoon);
 
-    glColor3f(0.5,0.8,0.2);
-    glBegin(GL_POLYGON);
+    //Луг
     float radiusField = 2.0;
     float centerXField = 0.0;
     float centerYField = -1.75;
-    for (int i = 0; i < numSegments; i++){
-        float alpha = i * (M_PI / (numSegments - 1));
-        float x = centerXField + radiusField * cos(alpha);
-        float y = centerYField + radiusField * sin(alpha);
-        glVertex2f(x, y);
-    }
-    glEnd();
+    glColor3f(0.5,0.8,0.2);
+    drawCircle(radiusField, centerXField, centerYField);
+
+    //Свинки
+    drawPig(0.75, 0);
+    drawPig(-0.1, 0.3);
+    drawPig(-0.45, -0.4);
+
+    //Цветы
+    drawFlower(-0.7, -0.5);
+    drawFlower(0.0, -0.4);
+    drawFlower(0.7, -0.6);
     glFlush();
 }
 
 void update(int value)
 {
     angle += 0.01;
-    centerXSun = radius * cos(angle);
-    centerYSun = radius * sin(angle);
-    centerXMoon = radius * cos(angle + M_PI);
-    centerYMoon = radius * sin(angle + M_PI);
+    centerXSun = orbitRadius * cos(angle);
+    centerYSun = orbitRadius * sin(angle);
+    centerXMoon = orbitRadius * cos(angle + M_PI);
+    centerYMoon = orbitRadius * sin(angle + M_PI);
 
     if (centerYSun < 0){
         isDay = false;
@@ -73,7 +149,6 @@ void update(int value)
     else if (centerYSun > 0){
         isDay = true;
     }
-
     glutPostRedisplay();
     glutTimerFunc(16, update, 0);
 }

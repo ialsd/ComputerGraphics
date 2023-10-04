22d5546b794dc92490c0e139934fad32de3c6736
ialsd <14mouse_14@mail.ru>
2023-10-04 14:31:33 +0500

    added sun and moon

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,25 +1,69 @@
 #include "GL/glut.h"
 #include <math.h>
 
+float radiusSun = 0.1;
+float centerXSun = -1.0;
+float centerYSun = 0.0;
+
+float centerXMoon = 1.0;
+float centerYMoon = 0.0;
+
+float angle = 0.0;
+float radius = 1.0;
+int numSegments = 100;
+
 void displayMe(void)
 {
     glClearColor(0.69, 0.93, 0.93, 1);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glColor3f(0.98,0.9,0.08);
+    glBegin(GL_POLYGON);
+    for (int i = 0; i < numSegments; i++)
+    {
+        float theta = 2.0f * M_PI * float(i) / float(numSegments);
+        float x = radiusSun * cosf(theta);
+        float y = radiusSun * sinf(theta);
+        glVertex2f(x + centerXSun, y + centerYSun);
+    }
+    glEnd();
+
+    glColor3f(1.0, 1.0, 0.6);
+    glBegin(GL_POLYGON);
+    for (int i = 0; i < numSegments; i++)
+    {
+        float theta = 2.0f * M_PI * float(i) / float(numSegments);
+        float x = radiusSun * cosf(theta);
+        float y = radiusSun * sinf(theta);
+        glVertex2f(x + centerXMoon, y + centerYMoon);
+    }
+    glEnd();
+
     glColor3f(0.5,0.8,0.2);
     glBegin(GL_POLYGON);
-    float radius = 2.0;
-    float centerX = 0.0;
-    float centerY = -2.0;
-    int numVertices = 100;
-    for (int i = 0; i < numVertices; i++)
+    float radiusField = 2.0;
+    float centerXField = 0.0;
+    float centerYField = -1.75;
+    for (int i = 0; i < numSegments; i++)
     {
-        float angle = i * (M_PI / (numVertices - 1));
-        float x = centerX + radius * cos(angle);
-        float y = centerY + radius * sin(angle);
+        float alpha = i * (M_PI / (numSegments - 1));
+        float x = centerXField + radiusField * cos(alpha);
+        float y = centerYField + radiusField * sin(alpha);
         glVertex2f(x, y);
     }
     glEnd();
     glFlush();
+}
+
+void update(int value)
+{
+    angle += 0.01;
+    centerXSun = radius * cos(angle);
+    centerYSun = radius * sin(angle);
+    centerXMoon = radius * cos(angle + M_PI);
+    centerYMoon = radius * sin(angle + M_PI);
+    glutPostRedisplay();
+    glutTimerFunc(16, update, 0);
 }
 
 int main(int argc, char** argv)
@@ -30,6 +74,7 @@ int main(int argc, char** argv)
     glutInitWindowPosition(100, 100);
     glutCreateWindow("lab1");
     glutDisplayFunc(displayMe);
+    glutTimerFunc(0, update, 0);
     glutMainLoop();
     return 0;
 }
